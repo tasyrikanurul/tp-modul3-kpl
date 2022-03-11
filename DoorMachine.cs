78a837534c5314701096d1161ba0cabb92a3484e
tasyrikanurul <100851839+tasyrikanurul@users.noreply.github.com>
2022-03-11 17:23:26 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302201131
+{
+    internal class DoorMachine
+    {
+        enum State {Terkunci, Terbuka};
+        public static void Main()
+        {
+            State state = State.Terkunci;
+            string[] kondisipintu = { "Terkunci", "Terbuka" };
+            while (state != State.Terkunci) ;
+            {
+                Console.Write("Pintu tidak terkunci");
+
+            }
+            while (state != State.Terbuka) ;
+            {
+                Console.Write("Pintu terkunci");
+            }
+        }
+    }
+}
